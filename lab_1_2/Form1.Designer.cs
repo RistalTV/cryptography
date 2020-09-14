441c6a9a95fb92b1cf4aaaa98a79540a3fe6dbda
Ristal <ristaltvgames@gmail.com>
2020-09-14 15:08:45 +0300

    Закончил верстать цезаря

--- lab_1_2/Form1.Designer.cs
+++ lab_1_2/Form1.Designer.cs
@@ -46,8 +46,19 @@ namespace lab_1_2
             this.panelMethod_2 = new System.Windows.Forms.Panel();
             this.panelMethod_1 = new System.Windows.Forms.Panel();
             this.l1_panelMainRight = new System.Windows.Forms.Label();
+            this.l1_panelMethod_1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_k = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbRU = new System.Windows.Forms.RadioButton();
+            this.rbEN = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panelMainLeft.SuspendLayout();
             this.panelMainRight.SuspendLayout();
+            this.panelMethod_1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMainLeft
@@ -225,8 +236,19 @@ namespace lab_1_2
             // 
             // panelMethod_1
             // 
+            this.panelMethod_1.Controls.Add(this.rbEN);
+            this.panelMethod_1.Controls.Add(this.rbRU);
+            this.panelMethod_1.Controls.Add(this.textBox2);
+            this.panelMethod_1.Controls.Add(this.textBox1);
+            this.panelMethod_1.Controls.Add(this.label4);
+            this.panelMethod_1.Controls.Add(this.textBox_k);
+            this.panelMethod_1.Controls.Add(this.label3);
+            this.panelMethod_1.Controls.Add(this.label2);
+            this.panelMethod_1.Controls.Add(this.label1);
+            this.panelMethod_1.Controls.Add(this.l1_panelMethod_1);
+            this.panelMethod_1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMethod_1.Enabled = false;
-            this.panelMethod_1.Location = new System.Drawing.Point(900, 0);
+            this.panelMethod_1.Location = new System.Drawing.Point(0, 0);
             this.panelMethod_1.MaximumSize = new System.Drawing.Size(816, 530);
             this.panelMethod_1.MinimumSize = new System.Drawing.Size(816, 530);
             this.panelMethod_1.Name = "panelMethod_1";
@@ -247,6 +269,112 @@ namespace lab_1_2
     "\n\r\n\r\n\r\n\r\nДля начала работы выберите нужный метод из списка";
             this.l1_panelMainRight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // l1_panelMethod_1
+            // 
+            this.l1_panelMethod_1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.l1_panelMethod_1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l1_panelMethod_1.Location = new System.Drawing.Point(0, 0);
+            this.l1_panelMethod_1.Name = "l1_panelMethod_1";
+            this.l1_panelMethod_1.Size = new System.Drawing.Size(816, 46);
+            this.l1_panelMethod_1.TabIndex = 0;
+            this.l1_panelMethod_1.Text = "Система Цезаря  с ключевым словом";
+            this.l1_panelMethod_1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 14.25F);
+            this.label1.Location = new System.Drawing.Point(492, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите ключевое слово:";
+            // 
+            // textBox_k
+            // 
+            this.textBox_k.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.textBox_k.Location = new System.Drawing.Point(496, 72);
+            this.textBox_k.MaxLength = 2;
+            this.textBox_k.Name = "textBox_k";
+            this.textBox_k.Size = new System.Drawing.Size(203, 32);
+            this.textBox_k.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите расскладку:";
+            // 
+            // rbRU
+            // 
+            this.rbRU.AutoSize = true;
+            this.rbRU.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbRU.Location = new System.Drawing.Point(32, 72);
+            this.rbRU.Name = "rbRU";
+            this.rbRU.Size = new System.Drawing.Size(130, 28);
+            this.rbRU.TabIndex = 3;
+            this.rbRU.TabStop = true;
+            this.rbRU.Text = "Кириллица";
+            this.rbRU.UseVisualStyleBackColor = true;
+            // 
+            // rbEN
+            // 
+            this.rbEN.AutoSize = true;
+            this.rbEN.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.rbEN.Location = new System.Drawing.Point(32, 106);
+            this.rbEN.Name = "rbEN";
+            this.rbEN.Size = new System.Drawing.Size(121, 28);
+            this.rbEN.TabIndex = 3;
+            this.rbEN.TabStop = true;
+            this.rbEN.Text = "Латиница";
+            this.rbEN.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 14.25F);
+            this.label3.Location = new System.Drawing.Point(7, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Введите ключевое слово:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.textBox1.Location = new System.Drawing.Point(0, 220);
+            this.textBox1.MaxLength = 9999;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(816, 93);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 14.25F);
+            this.label4.Location = new System.Drawing.Point(7, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Результат";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.textBox2.Location = new System.Drawing.Point(0, 437);
+            this.textBox2.MaxLength = 9999;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(816, 93);
+            this.textBox2.TabIndex = 2;
+            // 
             // MainForm
             // 
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
@@ -262,6 +390,8 @@ namespace lab_1_2
             this.panelMainLeft.ResumeLayout(false);
             this.panelMainLeft.PerformLayout();
             this.panelMainRight.ResumeLayout(false);
+            this.panelMethod_1.ResumeLayout(false);
+            this.panelMethod_1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -284,7 +414,16 @@ namespace lab_1_2
         private System.Windows.Forms.Panel panelMethod_3;
         private System.Windows.Forms.Panel panelMethod_2;
         private System.Windows.Forms.Panel panelMethod_1;
-
+        private RadioButton rbEN;
+        private RadioButton rbRU;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox_k;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label l1_panelMethod_1;
     }
 }
 
