fab0880b24acde1a4b02565f14adeebbe7c80fe0
Ristal <ristaltvgames@gmail.com>
2020-09-15 14:21:01 +0300

    раскидал по файлам методы шифрования

---
+++ lab_1_2/method_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_1_2
+{
+    public partial class MainForm : Form
+    {
+
+
+
+    }
+}
